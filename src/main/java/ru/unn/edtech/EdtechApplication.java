1647a6b55e0d080f35bc8824acfc2a50d6071af3
Ilya <ilya.tatsenko@yandex.ru>
2026-02-22 13:46:08 +0300

    chore: init Spring Boot project with Flyway v1

---
+++ src/main/java/ru/unn/edtech/EdtechApplication.java
@@ -0,0 +1,13 @@
+package ru.unn.edtech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdtechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdtechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/unn/edtech/EdtechApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.unn.edtech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdtechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
